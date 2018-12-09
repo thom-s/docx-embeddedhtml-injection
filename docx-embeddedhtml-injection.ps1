5711fab135e961182628c03503610cd8b81badbb
Thomas <45467550+dube1@users.noreply.github.com>
2018-12-09 13:29:06 -0500

    Update docx-embeddedhtml-injection.ps1
    Turned code into a Inject-Docx function

--- docx-embeddedhtml-injection.ps1
+++ docx-embeddedhtml-injection.ps1
@@ -1,31 +1,37 @@
-# Parameters
-$working_path = "C:\Users\example\Documents\" # Path of document
-$original_docx = "test.docx"                  # Name of existing document (in path)
-$html_block = ""                              # HTML you want to insert in the embeddedHTML document
+function Inject-Docx{
+    param(
+        [string]$Path,      # Full path of document
+        [string]$HtmlBlock, # HTML you want to insert in the embeddedHTML document
+        [string]$DestinationName = "output.docx"
+    )
 
-# Open the document as an archive
+    $working_path = Split-Path $Path -Parent     # Path of document
+    $original_docx = Split-Path $Path -Leaf       # Name of existing document (in path)
 
-Rename-Item -Path $working_path$original_docx -NewName "archive.zip"
+    # Open the document as an archive
 
-Expand-Archive "$working_path\archive.zip" -DestinationPath "$working_path\archive"
+    Rename-Item -Path $Path -NewName "archive.zip"
 
-Rename-Item -Path "$working_path\archive.zip" -NewName $original_docx
+    Expand-Archive "$working_path\archive.zip" -DestinationPath "$working_path\archive"
 
-# Parse the XML file
+    Rename-Item -Path "$working_path\archive.zip" -NewName $original_docx
 
-$xml = New-Object XML
-$xml.Load("$working_path\archive\word\document.xml")
-$xml.SelectNodes("//*") | ForEach-Object {
-    if($_.embeddedHtml){
-        $_.SetAttribute("embeddedHtml",$html_block)
+    # Parse the XML file
+
+    $xml = New-Object XML
+    $xml.Load("$working_path\archive\word\document.xml")
+    $xml.SelectNodes("//*") | ForEach-Object {
+        if($_.embeddedHtml){
+            $_.SetAttribute("embeddedHtml",$HtmlBlock)
+        }
     }
+    $xml.Save("$working_path\archive\word\document.xml")
+
+    # Close back the file
+
+    Compress-Archive -Path "$working_path\archive\*" -DestinationPath "$working_path\archive.zip"
+
+    Remove-Item -Path "$working_path\archive" -Force -Recurse
+
+    Rename-Item -Path "$working_path\archive.zip" -NewName $DestinationName
 }
-$xml.Save("$working_path\archive\word\document.xml")
-
-# Close back the file
-
-Compress-Archive -Path "$working_path\archive\*" -DestinationPath "$working_path\archive.zip"
-
-Remove-Item -Path "$working_path\archive" -Force -Recurse
-
-Rename-Item -Path "$working_path\archive.zip" -NewName "output.docx"
